3da1e576e61883f038769b6710f299f550ef86fa
Yurii Myronov <yuramiron16@gmail.com>
2025-09-05 15:39:34 +0200

    init nuxt

---
+++ server/api/websocket.ts
@@ -0,0 +1,21 @@
+import type { Peer, Message } from 'crossws'
+
+const room = 'ROOM'
+export default defineWebSocketHandler({
+  open(peer) {
+    peer.subscribe(room)
+    peer.publish(room, `Another user joined the chat ${peer.id}`)
+  },
+  message(peer, message) {
+    onCalc(peer, message)
+    peer.publish(room, message.text())
+  }
+})
+function onCalc(peer: Peer, message: Message) {
+  if(message.text().startsWith('calc ')){
+    const equation = message.text().replace('calc ', '')
+    // TODO: UNSAFE - DO NOT DO IN PROD, CAN LEAD TO XSS/RCI
+    const result = eval(equation)
+    peer.send(`The result of "${equation}" is: ${result}`)
+  }
+}
