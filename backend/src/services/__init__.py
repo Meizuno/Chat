9178f780c0dc2c92a114f625f863d8c7176e1a30
Yurii Myronov <yuramiron16@gmail.com>
2025-10-13 21:27:11 +0200

    rename pg services

--- backend/src/services/__init__.py
+++ backend/src/services/__init__.py
@@ -1,3 +1,3 @@
-from src.services.pg import get_pg_engine, get_pg_session
+from src.services.pg_connection import get_pg_engine, get_pg_session
 from src.services.redis_connection import get_redis
 from src.services.message import create_message
