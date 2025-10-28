a3db2b0137d86ca64f4a1cd0e86e266c89d03edd
Andrey Sokolov <info@informix.pro>
2025-10-28 16:54:34 +0100

    init template

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-full">
+    <UEmpty
+      icon="i-lucide-file"
+      title="No projects found"
+      description="It looks like you haven't added any projects. Create one to get started."
+    />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ frontend/prettier.config.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  // Print trailing commas wherever possible in multi-line comma-separated syntax. (Set to "none" to disable).
+  trailingComma: 'none',
+
+  // Specify the number of spaces per indentation-level.
+  tabWidth: 2,
+
+  // Print semicolons at the ends of statements.
+  semi: false,
+
+  // Use single quotes instead of double quotes.
+  singleQuote: true,
+
+  // Specify the line length that the printer will wrap on.
+  printWidth: 80,
+
+  // Print spaces between brackets in object literals.
+  bracketSpacing: true,
+
+  // Specify the end of line used by Prettier.
+  endOfLine: 'auto',
+
+  // Omit parens when possible. Example: `x => x`
+  arrowParens: 'avoid',
+
+  // Control how whitespace is handled in HTML.
+  htmlWhitespaceSensitivity: 'ignore',
+
+  // Put the `>` of a multi-line HTML tag on a new line.
+  bracketSameLine: false,
+
+  // Put each attribute on a new line.
+  singleAttributePerLine: true
+}
